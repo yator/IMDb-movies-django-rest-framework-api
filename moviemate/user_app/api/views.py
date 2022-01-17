3cd3c5b412bcb9f630a172d2fcd977edfc1ba1f2
yator <abelkipruto4@gmail.com>
2022-01-17 15:58:48 +0300

    test units

--- moviemate/user_app/api/views.py
+++ moviemate/user_app/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-# from user_app import models
+from user_app import models
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -29,15 +29,15 @@ def registration_view(request):
             data['username'] =account.username
             data['email'] = account.email
 
-            # token=Token.objects.get(user=account).key
+            token=Token.objects.get(user=account).key
 
-            # data['token']=token
+            data['token']=token
 
-            refresh = RefreshToken.for_user(account)
-            data['token'] = {
-                   'refresh': str(refresh),
-                   'access': str(refresh.access_token),
-                  }
+            # refresh = RefreshToken.for_user(account)
+            # data['token'] = {
+            #        'refresh': str(refresh),
+            #        'access': str(refresh.access_token),
+            #       }
 
 
 
@@ -46,5 +46,5 @@ def registration_view(request):
             data =serializer.errors    
 
 
-        return Response(data)
+        return Response(data ,status=status.HTTP_201_CREATED)
 
--- moviemate/watch_list/tests.py
+++ moviemate/watch_list/tests.py
@@ -1,3 +1,94 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from watch_list.api import serializers
+from watch_list import models
+
+
+class StreamPlatformTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Password@123")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="Netflix", 
+                                about="#1 Platform", website="https://www.netflix.com")
+
+    def test_streamplatform_create(self):
+        data = {
+            "name": "Netflix",
+            "about": "#1 Streaming Platform",
+            "website": "https://netflix.com"
+        }
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+
+class WatchListTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Password@123")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="Netflix", 
+                                about="#1 Platform", website="https://www.netflix.com")
+        self.watchlist = models.WatchList.objects.create(platform=self.stream, title="Example Movie",
+                                storyline="Example Movie", active=True)
+
+    def test_watchlist_create(self):
+        data = {
+            "platform": self.stream,
+            "title": "Example Movie",
+            "storyline": "Example Story",
+            "active": True
+        }
+        response = self.client.post(reverse('movie-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('movie-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_watchlist_ind(self):
+        response = self.client.get(reverse('movie-detail', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.count(), 1)
+        self.assertEqual(models.WatchList.objects.get().title, 'Example Movie')
+
+
+class ReviewTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Password@123")
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name="Netflix", 
+                                about="#1 Platform", website="https://www.netflix.com")
+        self.watchlist = models.WatchList.objects.create(platform=self.stream, title="Example Movie",
+                                storyline="Example Movie", active=True)
+        self.watchlist2 = models.WatchList.objects.create(platform=self.stream, title="Example Movie",
+                                storyline="Example Movie", active=True)
+        self.review = models.Review.objects.create(review_user=self.user, rating=5, description="Great Movie", 
+                                watchlist=self.watchlist2, active=True)
+    
+
+    def test_review_create_unauth(self):
+        data = {
+            "review_user": self.user,
+            "rating": 5,
+            "description": "Great Movie!",
+            "watchlist": self.watchlist,
+            "active": True
+        }
+
+        self.client.force_authenticate(user=None)
+        response = self.client.post(reverse('review-create', args=(self.watchlist.id,)), data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
